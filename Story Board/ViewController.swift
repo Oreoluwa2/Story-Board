2e894458052eb69a1b7e4365217cc997e80f057c
Precious <oreoluwaomomofe4@gmail.com>
2020-07-10 14:39:18 -0500

    this is a one page app

---
+++ Story Board/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Story Board
+//
+//  Created by Oreoluwa Omomofe on 7/9/20.
+//  Copyright © 2020 Oreoluwa Omomofe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
